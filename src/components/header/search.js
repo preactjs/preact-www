98ebafc701c7e4cda2152735db2810e33a939381
Jason Miller <jason@developit.ca>
2016-11-17 23:23:26 -0500

    Fix docsearch HMR issue (it needs to be a singleton because the lib is)

--- src/components/header/search.js
+++ src/components/header/search.js
@@ -2,15 +2,15 @@ import { h, Component } from 'preact';
 import style from './style';
 import config from '../../config';
 
-let counter = 0;
+let docsearchInstance, input;
 
 export default class Search extends Component {
-	id = `docsearch-input-${++counter}`;
+	id = 'docsearch-input';
 
 	loaded = () => {
 		let { docsearch } = window;
-		if (docsearch) {
-			docsearch({
+		if (docsearch && !docsearchInstance) {
+			docsearchInstance = docsearch({
 				apiKey: config.docsearch.apiKey,
 				indexName: config.docsearch.indexName,
 				inputSelector: `#${this.id}`,
@@ -26,32 +26,38 @@ export default class Search extends Component {
 	}
 
 	componentDidMount() {
-		setTimeout( () => {
-			if (!window.docsearch) {
-				let head = document.head || document.querySelector('head');
+		input = input || document.getElementById(this.id);
 
-				let link = document.createElement('link');
-				link.rel = 'stylesheet';
-				link.href = '//cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css';
-				head.appendChild(link);
+		if (!input) {
+			input = document.createElement('input');
+			input.required = true;
+			input.id = this.id;
+			input.className = style.searchBox;
+			this.base.appendChild(input);
 
-				let script = document.createElement('script');
-				script.src = '//cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js';
-				script.onload = script.onerror = this.loaded;
-				head.appendChild(script);
-			}
-		}, 500);
+			setTimeout( () => {
+				if (!window.docsearch) {
+					let head = document.head || document.querySelector('head');
+
+					let link = document.createElement('link');
+					link.rel = 'stylesheet';
+					link.href = '//cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css';
+					head.appendChild(link);
+
+					let script = document.createElement('script');
+					script.src = '//cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js';
+					script.onload = script.onerror = this.loaded;
+					head.appendChild(script);
+				}
+			}, 500);
+		}
+	}
+
+	componentWillUnmount() {
+		if (input) input.parentNode.removeChild(input);
 	}
 
 	render() {
-		return (
-			<div class={style.search}>
-				<input
-					id={this.id}
-					class={style.searchBox}
-					required
-				/>
-			</div>
-		);
+		return <div class={style.search} />;
 	}
 }
