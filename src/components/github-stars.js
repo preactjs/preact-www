f31d274c701c940f8c8f6aab2bde465b8405b09f
Jason Miller <jason@developit.ca>
2016-11-21 14:43:35 -0500

    Update github-stars size & small it down a bit

--- src/components/github-stars.js
+++ src/components/github-stars.js
@@ -1,16 +1,21 @@
 import { h, Component } from 'preact';
 import { memoize } from 'decko';
 
-const getRepoInfo = memoize( repo => fetch('https://api.github.com/repos/'+repo).then( r => r.json() ) );
+const githubStars = memoize( repo => fetch('//api.github.com/repos/'+repo)
+	.then( r => r.json() )
+	.then( d => d.stargazers || d.watchers )
+);
+
+// make available to homepage REPL demo
+window.githubStars = githubStars;
 
 export default class GithubStars extends Component {
 	state = {
 		stars: localStorage._stars || ''
 	};
 
-	setStars = data => {
-		let stars = data.stargazers || data.watchers;
-		if (stars!=this.state.stars) {
+	setStars = stars => {
+		if (stars && stars!=this.state.stars) {
 			localStorage._stars = stars;
 			this.setState({ stars });
 		}
@@ -18,17 +23,19 @@ export default class GithubStars extends Component {
 
 	componentDidMount() {
 		let { user, repo } = this.props;
-		getRepoInfo(user+'/'+repo).then(this.setStars);
+		githubStars(user+'/'+repo).then(this.setStars);
 	}
 
-	render({ user, repo, children }, { stars }) {
-		let url = `https://github.com/${user}/${repo}/`;
+	render({ user, repo, simple, children }, { stars }) {
+		let url = `//github.com/${user}/${repo}/`;
+		if (simple) return (
+			<a href={url} class="stars" target="_blank">⭐️ {stars} Stars</a>
+		);
 		return (
 			<span class="github-btn">
 				<a class="gh-btn" href={url} target="_blank" aria-label="Star on GitHub">
 					<span class="gh-ico" /> Star
 				</a>
-				{' '}
 				<a class="gh-count" href={url} target="_blank">
 					{stars ? Math.round(stars).toLocaleString() : children || '..'}
 				</a>
