203a3ff688efa265364523c48c1cbf8eef722acf
Hassan Bazzi <contact@hassanbazzi.com>
2019-02-18 14:29:49 -0500

    fix path

--- src/prerender.js
+++ src/prerender.js
@@ -3,9 +3,9 @@ const { resolve } = require('path');
 const flatMap = require('flatmap');
 const config = require('./config.json');
 
-const routes = flatMap(config.nav, arr => arr.path ? {path: arr.path == '/' ? '/index' : arr.path, name: arr.name} : arr.routes);
+const routes = flatMap(config.nav, arr => arr.path ? {path: arr.path , name: arr.name} : arr.routes);
 
 module.exports = routes.map( route => ({
 	url: route.path,
-	data: fs.readFileSync( resolve(__dirname, '../', `content${route.path}.md`))
+	data: fs.readFileSync( resolve(__dirname, '../', `content${route.path == '/' ? '/index' : route.path}.md`))
 }));
