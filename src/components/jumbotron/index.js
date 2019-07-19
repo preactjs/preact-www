cb08fbd015afcb4699a4bef720778cbf44644597
Marvin Hagemeister <hello@marvinh.dev>
2019-07-19 12:54:50 +0200

    Restyle jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -2,9 +2,10 @@ import cx from '../../lib/cx';
 import style from './style';
 
 const Jumbotron = ({ children, class: c, ...props }) => (
-	<div class={cx(style.jumbotron, 'full-width', c)} {...props}>
-		{children}
-	</div>
+	<header class={cx(style.jumbotron, 'full-width', c)} {...props}>
+		<div class={style.stripes} />
+		<div class={style.content}>{children}</div>
+	</header>
 );
 
 export default Jumbotron;
