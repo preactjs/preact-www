c5748e536d2ed52c7f80ae62f4a1bf0bfe7e5092
Marvin Hagemeister <hello@marvinh.dev>
2019-07-20 14:17:10 +0200

    Refactor header + add social links

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,6 +6,8 @@ import style from './style';
 import { useStore } from '../store-adapter';
 import config from '../../config';
 import { useCallback, useEffect, useState } from 'preact/hooks';
+import ReleaseLink from './gh-version';
+import Corner from './corner';
 
 const LINK_FLAIR = {
 	logo: InvertedLogo
@@ -18,14 +20,25 @@ export default function Header() {
 
 	useEffect(() => {
 		if (open) setOpen(false);
-	}, [url, open]);
+	}, [url]);
 
 	return (
 		<header class={cx(style.header, open && style.open)}>
-			<Nav routes={config.nav} current={url} />
-			<Search />
-			<Hamburgler open={open} onClick={toggle} />
-			<Corner />
+			<div class={style.inner}>
+				<Nav class={style.nav} routes={config.nav} current={url} />
+				<Search />
+				<div class={style.social}>
+					<ReleaseLink class={cx(style.socialItem, style.release)} />
+					<a class={style.socialItem} href="https://github.com/preactjs/preact">
+						<img src="/assets/github.svg" alt="GitHub" />
+					</a>
+					<a class={style.socialItem} href="https://twitter.com/preactjs">
+						<img src="/assets/twitter.svg" alt="Twitter" />
+					</a>
+				</div>
+				<Hamburgler open={open} onClick={toggle} />
+				<Corner />
+			</div>
 		</header>
 	);
 }
@@ -117,18 +130,3 @@ const getRouteIdent = route =>
 	(route.name || route.title || route.url)
 		.toLowerCase()
 		.replace(/[^a-z0-9]/i, '');
-
-const Corner = () => (
-	<a
-		href="https://opencollective.com/preact"
-		target="_blank"
-		rel="noopener noreferrer"
-		class={style.corner}
-	>
-		<div class={style.cornerText}>
-			Help
-			<br />
-			Support Us !
-		</div>
-	</a>
-);
