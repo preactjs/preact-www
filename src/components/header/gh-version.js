4a47188d7153bcbfc83ef53fd8ab59c5603d5230
Jason Miller <jason@developit.ca>
2019-07-28 23:09:29 -0400

    Prerender version URL and make it fail back to master.

--- src/components/header/gh-version.js
+++ src/components/header/gh-version.js
@@ -3,14 +3,26 @@ import { useEffect, useState } from 'preact/hooks';
 import { fetchRelease } from '../../lib/github';
 import config from '../../config.json';
 
+let VERSION = 'v10';
+const URL = 'https://github.com/' + config.repo;
+
+if (PRERENDER) {
+	VERSION = require('../../../package.json').dependencies.preact.replace('^','');
+}
+
 export default function ReleaseLink(props) {
-	const [version, setVersion] = useState('unknown');
-	const [url, setUrl] = useState('#');
+	const [version, setVersion] = useState();
+	const [url, setUrl] = useState(URL);
 	useEffect(() => {
-		fetchRelease(config.repo).then(d => {
-			setVersion(d.version[0] === 'v' ? d.version : `v${d.version}`);
-			setUrl(d.url);
-		});
+		fetchRelease(config.repo)
+			.catch(() => ({
+				version: VERSION,
+				url: URL
+			}))
+			.then(d => {
+				setVersion(d.version[0] === 'v' ? d.version : `v${d.version}`);
+				setUrl(d.url);
+			});
 	}, []);
 
 	return (
