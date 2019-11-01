3df3dca7752ddfe3c24a29d9e1da25d4165d4988
Marvin Hagemeister <hello@marvinh.dev>
2019-11-01 14:33:49 +0100

    Filter release list by semver to get latest

--- src/components/header/gh-version.js
+++ src/components/header/gh-version.js
@@ -1,12 +1,30 @@
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import { fetchRelease } from '../../lib/github';
+import config from '../../config.json';
 
 const VERSION = '10.0.4';
 const URL = `https://github.com/preactjs/preact/releases/tag/${VERSION}`;
 
 export default function ReleaseLink(props) {
+	const [url, setUrl] = useState(URL);
+	const [version, setVersion] = useState(VERSION);
+
+	useEffect(() => {
+		fetchRelease(config.repo)
+			.catch(() => ({
+				version: VERSION,
+				url: URL
+			}))
+			.then(d => {
+				setVersion(d.version);
+				setUrl(d.url);
+			});
+	}, []);
+
 	return (
-		<a href={URL} {...props}>
-			v{VERSION}
+		<a href={url} {...props}>
+			v{version}
 		</a>
 	);
 }
