bd3d5c487404cc496cf0586dee08a58b6891a9cd
38elements <38elements@users.noreply.github.com>
2020-01-16 07:50:07 +0900

    Add description to Twitter card (#519)

--- src/prerender.js
+++ src/prerender.js
@@ -1,22 +1,51 @@
-// const fs = require('fs');
-// const { resolve } = require('path');
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
 const flatMap = require('flatmap');
 const config = require('./config.json');
 
+// https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup
+const MAX_DESCRIPTION_LENGTH = 201;
+
 if (process.env.PRERENDER_HOME) {
-	module.exports = [{
-		url: config.nav[0].path,
-		title: config.nav[0].title
-	}];
-}
-else {
+	module.exports = [
+		{
+			url: config.nav[0].path,
+			title: config.nav[0].title
+		}
+	];
+} else {
 	const routes = flatMap(config.nav.concat(config.docs), arr =>
 		arr.path ? { path: arr.path, name: arr.name } : arr.routes
 	);
 
-	module.exports = routes.map(route => ({
-		url: route.path,
-		title: route.name && route.name.en || route.name
-		// ,data: fs.readFileSync(resolve(__dirname, '../', `content${route.path == '/' ? '/index' : route.path}.md`), 'utf8')
-	}));
+	module.exports = routes.map(route => {
+		const content = fs.readFileSync(
+			path.resolve(
+				__dirname,
+				'../',
+				`content/en/${route.path == '/' ? 'index' : route.path}.md`
+			),
+			{ encoding: 'utf8' }
+		);
+		const name = (route.name && route.name.en) || route.name;
+		const title = !name || name === 'Preact' ? 'Preact' : `${name} – Preact`;
+		const contentBody = content.split('---')[2];
+		let description;
+		if (title === 'Preact' || contentBody === undefined) {
+			description = 'Fast 3kB alternative to React with the same modern API.';
+		} else {
+			const _contentBody = contentBody.replace(/#.*\n/, '');
+			description = marked(_contentBody)
+				.replace(/<[^>]*>?/gm, '')
+				.trimStart()
+				.replace(/\n/gm, ' ')
+				.substring(0, MAX_DESCRIPTION_LENGTH);
+		}
+		return {
+			url: route.path,
+			title,
+			description
+		};
+	});
 }
