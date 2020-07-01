297a19b837159fdb07cb05eec5c264320dbd2c0b
Jeremy <jeremy.k.coleman@outlook.com>
2020-07-01 12:25:55 -0400

    move referrer check from constructor to lifecycle
    fixes "run in repl" links

--- src/components/controllers/repl/index.js
+++ src/components/controllers/repl/index.js
@@ -26,10 +26,9 @@ export default class Repl extends Component {
 		code: localStorageGet('preact-www-repl-code') || codeExample
 	};
 
-	constructor(props, context) {
-		super(props, context);
-		if (props.code) {
-			this.state.code = props.code;
+	componentWillMount() {
+		if (this.props.code) {
+			this.receiveCode(this.props.code);
 		}
 	}
 
