a748693babb95f260970500215cc11678c851218
Marvin Hagemeister <hello@marvinh.dev>
2020-08-11 22:50:16 +0200

    Add footer page navigation

--- src/components/content-region.js
+++ src/components/content-region.js
@@ -1,5 +1,7 @@
 import Markup from 'preact-markup';
 import widgets from './widgets';
+import style from './content-region.less';
+import { useTranslation } from '../lib/i18n';
 
 const COMPONENTS = {
 	...widgets,
@@ -16,9 +18,36 @@ const COMPONENTS = {
 	}
 };
 
-export default function ContentRegion({ content, ...props }) {
+function SiblingNav({ route, lang, start }) {
+	let title = '';
+	let url = '';
+	if (route) {
+		url = route.path.toLowerCase();
+		title =
+			typeof route.name === 'object'
+				? route.name[lang || 'en']
+				: route.name || route.title;
+	}
+	const label = useTranslation(start ? 'previous' : 'next');
+
 	return (
-		<content-region {...props}>
+		<a class={style.nextLink} data-dir-end={!start} href={url}>
+			{start && <span class={style.icon}>&larr;&nbsp;</span>}
+			{!start && <span class={style.icon}>&nbsp;&rarr;</span>}
+			<span class={style.nextInner}>
+				<span class={style.nextTitle}>
+					<span class={style.nextTitleInner}>{title}</span>
+				</span>
+				<span class={style.nextUrl}>{label}</span>
+			</span>
+		</a>
+	);
+}
+
+export default function ContentRegion({ content, ...props }) {
+	const hasNav = !!(props.next || props.prev);
+	return (
+		<content-region name={props.name} data-page-nav={hasNav}>
 			{content && (
 				<Markup
 					// key={content}
@@ -27,6 +56,20 @@ export default function ContentRegion({ content, ...props }) {
 					trim={false}
 					components={COMPONENTS}
 				/>
+			)}
+			{hasNav && (
+				<div class={style.nextWrapper}>
+					{props.prev ? (
+						<SiblingNav start lang={props.lang} route={props.prev} />
+					) : (
+						<span />
+					)}
+					{props.next ? (
+						<SiblingNav lang={props.lang} route={props.next} />
+					) : (
+						<span />
+					)}
+				</div>
 			)}
 		</content-region>
 	);
