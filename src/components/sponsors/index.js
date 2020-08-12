26ece0a4cf4a8a520876e8b8a7d784ae777ced2b
Jason Miller <developit@users.noreply.github.com>
2020-08-12 13:06:03 -0400

    Update index.js

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -12,12 +12,12 @@ export default function Sponsors() {
 	return (
 		<ul class={styles.sponsorList}>
 			<li class={styles.sponsorItem}>
-				<a href="https://amp.dev/" title="AMP" target="_blank" rel="noopener">
+				<a href="https://amp.dev/" title="AMP" target="_blank" rel="noopener noreferrer">
 					<img src={AMPSvg} alt="AMP" width="150" height="150" style="height: 4rem; width: auto;" />
 				</a>
 			</li>
 			<li class={styles.sponsorItem}>
-				<a href="https://webflow.com/" title="webflow" target="_blank" rel="noopener">
+				<a href="https://webflow.com/" title="webflow" target="_blank" rel="noopener noreferrer">
 					<img src={WebflowSvg} alt="webflow" width="300" height="75" style="height:3rem; width: auto;" />
 				</a>
 			</li>
