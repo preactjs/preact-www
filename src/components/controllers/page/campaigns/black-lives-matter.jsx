8cb2c6c1ceaa53bd02eab1cd27f69660b750ab7c
Marvin Hagemeister <hello@marvinh.dev>
2020-10-22 13:40:32 +0200

    Update BLM banner + add EndSARS

--- src/components/controllers/page/campaigns/black-lives-matter.jsx
+++ src/components/controllers/page/campaigns/black-lives-matter.jsx
@@ -3,42 +3,24 @@ import s from './BlackLivesMatter.less';
 export function BlackLivesMatterBanner() {
 	return (
 		<div class={s.root}>
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://blacklivesmatter.com/"
-				class={s.link}
-			>
-				Black Lives Matter
-			</a>
-			. Support{' '}
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://www.joincampaignzero.org/"
-				class={s.link}
-			>
-				Campaign Zero
-			</a>
-			, the{' '}
+			Support{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
 				href="https://support.eji.org/give/153413/#!/donation/checkout"
 				class={s.link}
 			>
-				Equal&nbsp;Justice&nbsp;Initiative
-			</a>
-			, and{' '}
+				#Black Lives Matter
+			</a>{' '}
+			and{' '}
 			<a
 				target="_blank"
 				rel="noopener noreferrer"
-				href="https://bailfunds.github.io/"
 				class={s.link}
+				href="https://techcabal.com/2020/10/12/the-next-wave-vive-la-resistance/"
 			>
-				local bail funds
+				#EndSARS
 			</a>
-			.
 		</div>
 	);
 }
