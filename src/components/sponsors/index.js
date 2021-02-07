69242100539991e9d25fe6239b0b62ace0f1d0e6
Marvin Hagemeister <hello@marvinh.dev>
2021-02-07 23:50:55 +0100

    Add trivago as a sponsor

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -1,5 +1,6 @@
 import styles from './style.less';
 import AMPSvg from './amp.svg';
+import TrivagoSvg from './trivago.svg';
 import WebflowSvg from './webflow.svg';
 
 /**
@@ -12,13 +13,51 @@ export default function Sponsors() {
 	return (
 		<ul class={styles.sponsorList}>
 			<li class={styles.sponsorItem}>
-				<a href="https://amp.dev/" title="AMP" target="_blank" rel="noopener noreferrer">
-					<img src={AMPSvg} alt="AMP" width="150" height="150" style="height: 4rem; width: auto;" />
+				<a
+					href="https://amp.dev/"
+					title="AMP"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={AMPSvg}
+						alt="AMP"
+						width="150"
+						height="150"
+						style="height: 4rem; width: auto;"
+					/>
 				</a>
 			</li>
 			<li class={styles.sponsorItem}>
-				<a href="https://webflow.com/" title="webflow" target="_blank" rel="noopener noreferrer">
-					<img src={WebflowSvg} alt="webflow" width="300" height="75" style="height:3rem; width: auto;" />
+				<a
+					href="https://tech.trivago.com/opensource/"
+					title="trivago"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={TrivagoSvg}
+						alt="trivago"
+						width="150"
+						height="150"
+						style="height: 3.5rem; width: auto;"
+					/>
+				</a>
+			</li>
+			<li class={styles.sponsorItem}>
+				<a
+					href="https://webflow.com/"
+					title="webflow"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={WebflowSvg}
+						alt="webflow"
+						width="300"
+						height="75"
+						style="height:3rem; width: auto;"
+					/>
 				</a>
 			</li>
 		</ul>
