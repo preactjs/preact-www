f8d18536d4bc6c6eeaf447238977e95d78b84a4f
Muhun Kim <iam@muhun.kim>
2021-03-06 22:06:36 +0900

    Support non-alphanumeric characters in ToC
    ```js
    // Note: character range in regex is roughly "word characters including accented" (eg: bublé)
    const id = text
      // korean, Japanese (hiragana)
      .replace(/[^가-힣\u3041-\u3096]/g, '');
    ```
    
    If want to keep existing intent, the above code might be more suitable.
    
    But i haven't opinion yet, so I remove the one line that erased non-alphanumeric characters.
    
    Reference issue comment: https://github.com/preactjs/preact-www/issues/754#issuecomment-789785108

--- src/lib/content.js
+++ src/lib/content.js
@@ -159,7 +159,6 @@ function generateToc(markdown) {
 		const id = text
 			.toLowerCase()
 			.replace(/[\s-]+/g, '-')
-			.replace(/[^a-z0-9\u00C0-\u024F-]/g, '');
 		toc.push({ text, id, level });
 	}
 	return toc;
