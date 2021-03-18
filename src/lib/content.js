3a313c0dba30850606c400ac5b0b0d661aff2936
Jason Miller <developit@users.noreply.github.com>
2021-03-18 15:01:28 -0400

    lint nit

--- src/lib/content.js
+++ src/lib/content.js
@@ -158,7 +158,7 @@ function generateToc(markdown) {
 		// Note: character range in regex is roughly "word characters including accented" (eg: bublé)
 		const id = text
 			.toLowerCase()
-			.replace(/[\s-!()<>`'"&,]+/g, '-')
+			.replace(/[\s-!()<>`'"&,]+/g, '-');
 		toc.push({ text, id, level });
 	}
 	return toc;
