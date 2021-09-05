a3c0e9af028dfb76747feaa26b576f4e9ae675f8
Marvin Hagemeister <hello@marvinh.dev>
2021-09-05 12:16:51 +0200

    Add back missing meta tags

--- src/components/controllers/markdown-region.js
+++ src/components/controllers/markdown-region.js
@@ -1,17 +1,21 @@
 import EditThisPage from '../edit-button';
 import ContentRegion from '../content-region';
-import { useRoute } from 'preact-iso';
+import { useRoute, useLocation } from 'preact-iso';
 import { useTitle, useMeta, useTitleTemplate } from 'hoofd/preact';
 import style from './style.module.less';
 
 export function MarkdownRegion({ html, meta }) {
 	const { params } = useRoute();
+	const { url } = useLocation();
 
 	useTitleTemplate(
 		'%s | Preact: Fast 3kb React alternative with the same ES6 API. Components & Virtual DOM.'
 	);
 	useTitle(meta.title);
 	useMeta({ name: 'description', content: meta.description });
+	useMeta({ name: 'og:description', content: meta.description });
+	useMeta({ name: 'og:title', content: meta.title });
+	useMeta({ name: 'og:url', content: url });
 
 	const showTitle = 'show_title' in meta ? !!meta.show_title : true;
 	const canEdit = showTitle && params.name != '404';
