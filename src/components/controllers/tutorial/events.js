fe31f1d964ff6611b725360a421c969ed7e4a3e5
JoviDeCroock <decroockjovi@gmail.com>
2021-12-04 12:26:02 +0100

    complete vdom and events tutorial

---
+++ src/components/controllers/tutorial/events.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-console  */
+import { Fragment } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import CodeBlock from '../../code-block';
+
+const EventsTutorial = () => {
+	const [called, setCalled] = useState(false);
+
+	useEffect(() => {
+		let prevConsoleLog = console.log;
+		console.log = function(...args) {
+			setCalled(true);
+			prevConsoleLog.call(window, ...args);
+		};
+
+		return () => {
+			console.log = prevConsoleLog;
+		};
+	}, []);
+
+	return (
+		<Fragment>
+			<h2>2. Events</h2>
+			<p>
+				Next up we'll look at how events work in Preact, events are instrumental
+				to us having an interactive application, this is part of the DOM and
+				hence also part of our attributes (properties) model.
+			</p>
+			<p>
+				We can assign events to our dom-nodes with the "on" prefix, so if we
+				want to assign a click-handler to our button we can simply say "onClick"
+				which will instrument it to call a certain function when an event
+				occurs. Similar to doing the following yourself:
+			</p>
+
+			<CodeBlock>
+				<code class="language-js">
+					document.getElementById('my-button').addEventListener('click',
+					function() &#123; console.log('clicked') &#125;)
+				</code>
+			</CodeBlock>
+
+			<p>
+				To complete this let's assign a click-handler through jsx on our Button
+				and then click it.
+			</p>
+
+			{called && (
+				<p>
+					Congratulations! You just assigned your first event-handler through
+					Preact!
+				</p>
+			)}
+		</Fragment>
+	);
+};
+
+EventsTutorial.initialCode = `import { createElement } from 'preact';
+
+export default function App() {
+	return (
+		<div style={{ padding: 8 }}>
+			<p style={{ color: 'red' }}>Count:</p>
+			<button>Click me</button>
+		</div>
+	)
+}
+`;
+
+EventsTutorial.finalCode = `import { createElement } from 'preact';
+
+export default function App() {
+	const onClick = () => {
+		console.log('hi')
+	}
+
+	return (
+		<div style={{ padding: 8 }}>
+			<p style={{ color: 'red' }}>Count:</p>
+			<button>Click me</button>
+		</div>
+	)
+}
+
+`;
+
+export default EventsTutorial;
--- src/components/controllers/tutorial/index.js
+++ src/components/controllers/tutorial/index.js
@@ -4,6 +4,10 @@ import style from './style.module.less';
 import { ErrorOverlay } from '../repl/error-overlay';
 import { parseStackTrace } from '../repl/errors';
 
+// Steps
+import VDomTutorial from './vdom';
+import EventsTutorial from './events';
+
 export default class Tutorial extends Component {
 	state = {
 		loading: 'Loading Tutorial...',
@@ -23,16 +27,7 @@ export default class Tutorial extends Component {
 			this.Runner.worker.ping().then(() => {
 				this.setState({
 					loading: false,
-					code: `import { createElement } from 'preact';
-
-export default function App() {
-	return (
-		<div style={{ padding: 8 }}>
-			<p>Hello world</p>
-		</div>
-	)
-}
-`
+					code: VDomTutorial.initialCode
 				});
 			});
 		});
@@ -40,6 +35,30 @@ export default function App() {
 
 	onSuccess = () => {
 		this.setState({ error: null });
+	};
+
+	nextStep = () => {
+		switch (this.state.step) {
+			case 1: {
+				this.setState({
+					step: 2,
+					code: EventsTutorial.initialCode
+				});
+				break;
+			}
+		}
+	};
+
+	help = () => {
+		switch (this.state.step) {
+			case 1: {
+				this.setState({
+					step: 1,
+					code: VDomTutorial.finalCode
+				});
+				break;
+			}
+		}
 	};
 
 	render(_, { loading, code, error, example, copied }) {
@@ -52,78 +71,31 @@ export default function App() {
 				</ReplWrapper>
 			);
 
+		let step;
+
+		switch (this.state.step) {
+			case 1:
+				step = <VDomTutorial />;
+				break;
+			case 2:
+				step = <EventsTutorial />;
+				break;
+		}
+
 		return (
 			<ReplWrapper loading={!!loading}>
 				<div class={style.tutorialWrapper}>
 					<div class={style.tutorialWindow}>
-						<h2>1. Virtual DOM</h2>
-						<p>
-							You might've heard a lot of people talk about Virtual DOM
-							libraries, which might be confusing as how does it differ from our
-							real DOM? Well in a Virtual DOM library we'll keep a virtual
-							representation of your DOM-tree in-memory so we can "diff" them.
-							This means that when an update comes in we can see what the
-							differences between the previous and current Virtual DOM tree are
-							and then apply them to the actual DOM.
-						</p>
-						<p>
-							In this chapter we'll learn how we actually create one of those
-							Virtual DOM trees and make it translate to DOM for us. There are a
-							few ways of creating one of these trees:
-						</p>
-						<ul>
-							<li>
-								Leverage the <code>createElement</code> function exported from
-								preact
-							</li>
-							<li>
-								Write JSX, which is an HTML-like syntax that get transpiled by
-								Babel/ESBuild/... to <code>createElement</code>
-							</li>
-							<li>
-								Use the{' '}
-								<a href="https://github.com/developit/htm">htm library</a>
-							</li>
-						</ul>
+						{step}
 
-						<p>An example:</p>
-
-						<code>
-							import &#123; createElement &#125; from 'preact'; export default
-							function App() &#123; return createElement('p', &#123; class:
-							'my-paragraph' &#125;, 'hello world') &#125;
-						</code>
-
-						<p>
-							So what does the above code snipper do? Well we tell Preact to
-							render a paragraph element with the text hello-world. As the
-							first-prop is the type of element we want, the second are the
-							attributes we want to assign to it, in this case we are assigning
-							it a class named "my-paragraph" and the last attribute is the
-							child for this paragraph which is the text "hello world" in this
-							case. If we'd translate this to JSX we'd see:
-						</p>
-
-						<code>
-							import &#123; createElement &#125; from 'preact'; export default
-							function App() &#123; return &lt;p class="my-paragraph"&gt;Hello
-							world&lt;/p&gt; &#125;
-						</code>
-
-						<p>
-							On the right-hand side you'll see the code at the top and the
-							rendered result at the bottom, you'll see our hello-world
-							rendered. Now we want to give this some more... Power, let's wrap
-							our paragraph with a div and use the "style" attribute to give it
-							a padding of 8, we can use the style attribute like this:
-							style=&#123;&#123; property: value, &#125;&#125;. You can give it
-							some color as well if you like, play around with it, have fun!
-							Next up, let's add a "button" element as well and change text of
-							"hello world" to "count:"
-						</p>
-
-						<button>Help!</button>
-						<button>Next</button>
+						<div class={style.buttonContainer}>
+							<button class={style.helpButton} onClick={this.help}>
+								Help
+							</button>
+							<button class={style.nextButton} onClick={this.nextStep}>
+								Next
+							</button>
+						</div>
 					</div>
 
 					<div class={style.codeWindow}>
