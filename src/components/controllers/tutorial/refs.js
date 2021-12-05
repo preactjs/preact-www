2e1ecc8262656a13866fe999f62c62fe328dcfff
JoviDeCroock <decroockjovi@gmail.com>
2021-12-05 12:15:05 +0100

    finish refs

--- src/components/controllers/tutorial/refs.js
+++ src/components/controllers/tutorial/refs.js
@@ -1,4 +1,6 @@
 import { Fragment } from 'preact';
+import CodeBlock from '../../code-block';
+import style from './style.module.less';
 
 const RefsTutorial = () => (
 	<Fragment>
@@ -18,13 +20,66 @@ const RefsTutorial = () => (
 			to a dom-node?
 			<br />
 			Well Preact has a few special keywords that you can pass on a dom-node and
-			"ref" is one of them.
+			"ref" is one of them. Let's look at an example
+		</p>
+		<CodeBlock repl="false" class={style.codeContainer}>
+			<code class="language-jsx">
+				{`import { createElement } from 'preact';
+import { useRef } from 'preact/hooks';
+
+export default function App() {
+	const input = useRef();
+	return <input ref={input} />
+}`}
+			</code>
+		</CodeBlock>
+		<p>
+			now after this has rendered "input.current" will contain a reference to
+			this dom-node which we can use to for instance call
+			"input.current.focus()" when someone clicks a button.
+			<br />
+			Refs can be used for more than just storing dom-nodes as we can just
+			assign values like "x.current = y" but we'll see this in another chapter.
+			<br />
+			Now let's put this to practice, when we click our button we want to focus
+			the input!
 		</p>
 	</Fragment>
 );
 
-RefsTutorial.initialCode = ``;
+RefsTutorial.initialCode = `import { createElement } from 'preact';
+import { useRef } from 'preact/hooks';
 
-RefsTutorial.finalCode = ``;
+export default function App() {
+	function onClick() {
+
+	}
+
+	return (
+		<div>
+			<input initialValue="hello-world" />
+			<Button id="focus-input" onClick={onClick}>Focus input</Button>
+		</div>
+		);
+}
+`;
+
+RefsTutorial.finalCode = `import { createElement } from 'preact';
+import { useRef } from 'preact/hooks';
+
+export default function App() {
+	const input = useRef();
+	function onClick() {
+		input.current.focus();
+	}
+
+	return (
+		<div>
+			<input ref={input} initialValue="hello-world" />
+			<Button id="focus-input" onClick={onClick}>Focus input</Button>
+		</div>
+		);
+}
+`;
 
 export default RefsTutorial;
