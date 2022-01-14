ed61723733156201ccd1c3d5a04f1c4e7f072824
cristianbote <botecristian@yahoo.com>
2022-01-14 12:35:55 +0200

    Remove hand-rolling portal attempts

--- src/lib/tooltip/tooltip.js
+++ src/lib/tooltip/tooltip.js
@@ -1,14 +1,11 @@
-import { h, createElement, Fragment } from 'preact';
+import { h } from 'preact';
 import cx from '../cx';
 import './tooltip.css';
 
 export const WithTooltip = ({ children, tooltip, showTooltip = true }) => (
 	<div className={cx('tooltip-wrapper', showTooltip && 'tooltip-enabled')}>
 		<div className="tooltip-container">
-			{createElement(Fragment, {
-				children: <div className="tooltip-content">{tooltip}</div>,
-				_container: document.getElementById('overlay-stack')
-			})}
+			<div className="tooltip-content">{tooltip}</div>
 		</div>
 		{children}
 	</div>
