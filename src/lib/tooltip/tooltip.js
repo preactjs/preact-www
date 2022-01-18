8d315df1452c5a0d0a7fb6cb58ea3eb2fd2ff5a1
cristianbote <botecristian@yahoo.com>
2022-01-18 17:23:36 +0200

    Fix the tooltip behaviour

--- src/lib/tooltip/tooltip.js
+++ src/lib/tooltip/tooltip.js
@@ -1,12 +1,37 @@
 import { h } from 'preact';
+import { useRef, useState } from 'preact/hooks';
 import cx from '../cx';
 import './tooltip.css';
 
-export const WithTooltip = ({ children, tooltip, showTooltip = true }) => (
-	<div className={cx('tooltip-wrapper', showTooltip && 'tooltip-enabled')}>
-		<div className="tooltip-container">
-			<div className="tooltip-content">{tooltip}</div>
-		</div>
-		{children}
-	</div>
-);
+export const WithTooltip = ({ children, tooltip, showTooltip = true }) => {
+	const wrapper = useRef();
+	const [pos, setPos] = useState({ top: 0, left: 0 });
+
+	function handleMouseOver() {
+		const rect = wrapper.current.getBoundingClientRect();
+		setPos({ top: rect.top, left: rect.left });
+	}
+
+	return (
+		<>
+			<div
+				className={cx(showTooltip && 'tooltip-enabled')}
+				ref={wrapper}
+				onMouseOver={handleMouseOver}
+			>
+				{children}
+			</div>
+			{showTooltip ? (
+				<div
+					className="tooltip-container"
+					style={{
+						top: pos.top,
+						left: pos.left
+					}}
+				>
+					<div className="tooltip-content">{tooltip}</div>
+				</div>
+			) : null}
+		</>
+	);
+};
