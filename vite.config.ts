b091ade9a74968535203c6c9cb601d8152937d61
Marvin Hagemeister <hello@marvinh.dev>
2022-03-30 09:19:38 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import preact from '@preact/preset-vite';
 import inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
-	plugins: [inspect(), preact()]
+	build: {
+		manifest: true
+	},
+	plugins: [inspect(), preact(), markdown()]
 });
+
+function markdown(): Plugin {
+	const FRONT_MATTER_REG = /^\s*---\n\s*([\s\S]*?)\s*\n---\n(.*)/i;
+
+	return {
+		name: 'md',
+		configureServer(server) {
+			server.middlewares.use((req, res, next) => {
+				const url = req.url;
+				if (url && url.endsWith('.md')) {
+					const [, lang, file] = url.match(/^\/content\/(.*)\/(.*)\.md$/);
+					console.log(url, lang, file);
+				}
+				next();
+			});
+		},
+		transform(code, id) {
+			if (!id.includes('.md')) return;
+			console.log('transform', id);
+
+			let [, frontMatter] = code.match(FRONT_MATTER_REG) || [];
+
+			return;
+			meta = (frontMatter && JSON.parse(frontMatter)) || {};
+			console.error(code);
+		}
+	};
+}
