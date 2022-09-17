4b3580ca9cacfcd51b00a5dedc65ae0efb3a9a83
Ryan Christian <rchristian@ryanchristian.dev>
2022-09-17 05:13:19 -0500

    chore: Bump preact-cli, remove old size-plugin

--- preact.config.js
+++ preact.config.js
@@ -1,12 +1,10 @@
-import { resolve, join } from 'path';
+import { resolve } from 'path';
 import fs from 'fs';
 import delve from 'dlv';
 import CopyPlugin from 'copy-webpack-plugin';
-import Critters from 'critters-webpack-plugin';
 import yaml from 'yaml';
 import netlifyPlugin from 'preact-cli-plugin-netlify';
 import customProperties from 'postcss-custom-properties';
-import SizePlugin from 'size-plugin';
 // prettier-ignore
 
 export default function (config, env, helpers) {
@@ -24,8 +22,9 @@ export default function (config, env, helpers) {
 		config.entry.polyfills = resolve(__dirname, 'src/polyfills.js');
 	}
 
-	helpers.getPluginsByName(config, 'DefinePlugin')[0].plugin.definitions.PRERENDER = String(env.ssr===true);
-	helpers.getPluginsByName(config, 'DefinePlugin')[0].plugin.definitions['process.env.BRANCH'] = JSON.stringify(process.env.BRANCH);
+	const { plugin: definePlugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+	definePlugin.definitions.PRERENDER = String(env.ssr===true);
+	definePlugin.definitions['process.env.BRANCH'] = JSON.stringify(process.env.BRANCH);
 
 	// web worker HMR requires it
 	config.output.globalObject = 'self';
@@ -51,16 +50,6 @@ export default function (config, env, helpers) {
 		});
 	});
 
-	const critters = helpers.getPluginsByName(config, 'Critters')[0];
-	if (critters) {
-		config.plugins[critters.index] = new Critters({
-			preload: 'media',
-			additionalStylesheets: ['?.chunk.*.css'],
-			pruneSource: false,
-			compress: true
-		});
-	}
-
 	// Fix keyframes being minified to colliding names when using lazy-loaded CSS chunks
 	const optimizeCss = config.optimization && (config.optimization.minimizer || []).filter(plugin => /^OptimizeCssAssets(Webpack)?Plugin$/.test(plugin.constructor.name))[0];
 	if (optimizeCss) {
@@ -71,13 +60,6 @@ export default function (config, env, helpers) {
 		minSize: 1000
 	});
 
-	const sizePlugin = helpers.getPluginsByName(config, 'SizePlugin')[0];
-	if (sizePlugin) {
-		config.plugins[sizePlugin.index] = new SizePlugin({
-			publish: true,
-			filename: `size-plugin-${env.ssr?'ssr':'browser'}.json`
-		});
-	}
 	if (!env.ssr) {
 		// Find YAML FrontMatter preceeding a markdown document
 		const FRONT_MATTER_REG = /^\s*---\n\s*([\s\S]*?)\s*\n---\n/i;
@@ -86,43 +68,47 @@ export default function (config, env, helpers) {
 		const TITLE_REG = /^\s*#\s+(.+)\n+/;
 
 		// Converts YAML FrontMatter to JSON FrontMatter for easy client-side parsing.
-		config.plugins.push(new CopyPlugin([{
-			context: __dirname,
-			from: 'content',
-			to: 'content',
-			transform(content, path) {
-				if (typeof content !== 'string') {
-					content = content.toString('utf8');
-				}
-
-				const matches = content.match(FRONT_MATTER_REG);
-				if (!matches) return content;
-
-				const meta = yaml.parse('---\n'+matches[1].replace(/^/gm,'  ')+'\n') || {};
-				content = content.replace(FRONT_MATTER_REG, '');
-				if (!meta.title) {
-					let [,title] = content.match(TITLE_REG) || [];
-					if (title) {
-						content = content.replace(TITLE_REG, '');
-						meta.title = title;
+		config.plugins.push(new CopyPlugin({
+			patterns: [{
+				context: __dirname,
+				from: 'content',
+				to: 'content',
+				transform(content, path) {
+					if (typeof content !== 'string') {
+						content = content.toString('utf8');
 					}
-				}
 
-				content = '---\n' + JSON.stringify(meta) + '\n---\n' + content;
-				return content;
-			}
-		}, {
-			context: __dirname,
-			from: 'src/robots.txt',
-			to: 'robots.txt'
-		}]));
+					const matches = content.match(FRONT_MATTER_REG);
+					if (!matches) return content;
+
+					const meta = yaml.parse('---\n' + matches[1].replace(/^/gm, '  ') + '\n') || {};
+					content = content.replace(FRONT_MATTER_REG, '');
+					if (!meta.title) {
+						let [, title] = content.match(TITLE_REG) || [];
+						if (title) {
+							content = content.replace(TITLE_REG, '');
+							meta.title = title;
+						}
+					}
+
+					content = '---\n' + JSON.stringify(meta) + '\n---\n' + content;
+					return content;
+				}
+			}, {
+				context: __dirname,
+				from: 'src/robots.txt',
+				to: 'robots.txt'
+			}, {
+				context: __dirname,
+				from: 'src/_headers',
+				to: '_headers',
+				// Copy-Webpack-Plugin otherwise assumes it's a directory, which results in errors
+				toType: 'file'
+			}]
+		}));
 
 		netlifyPlugin(config, {
 			redirects: fs.readFileSync('src/_redirects', 'utf-8').trim().split('\n')
 		});
-
-		// For some reason, webpack CopyPlugin throws an error when trying to copy this using that plugin
-		fs.mkdirSync(join(__dirname, 'build'), { recursive: true });
-		fs.copyFileSync(join(__dirname, 'src/_headers'), join(__dirname, 'build/_headers'));
 	}
 }
