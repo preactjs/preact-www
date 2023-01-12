f3189c28269d85405a964d0d1dd0474e3ce83860
Andre Wiggins <andrewiggins@live.com>
2023-01-12 14:06:45 -0800

    Add new sponsor logos

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -1,14 +1,9 @@
 import styles from './style.module.less';
-import AMPSvg from './amp.svg';
 import TrivagoSvg from './trivago.svg';
-
-// TODO: Add
-// - Airbnb
-// - Chrome
-// - Etsy
-// - Scams.info
-// - Deno
-// - Songsterr
+import DenoSvg from './deno.svg';
+import ChromeSvg from './chrome.svg';
+import SongsterrSvg from './songsterr.svg';
+import ScamsInfo from './scamsinfo.png';
 
 /**
  * Sponsors on the main page is a unique selling point of our
@@ -21,16 +16,16 @@ export default function Sponsors() {
 		<ul class={styles.sponsorList}>
 			<li class={styles.sponsorItem}>
 				<a
-					href="https://amp.dev/"
-					title="AMP"
+					href="https://opencollective.com/2021-frameworks-fund"
+					title="Chrome Frameworks Fund"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<img
-						src={AMPSvg}
-						alt="AMP"
-						width="150"
-						height="100"
+						src={ChromeSvg}
+						alt="Chrome Frameworks Fund"
+						width="63"
+						height="63"
 						style="height: 3rem; width: auto;"
 					/>
 				</a>
@@ -47,6 +42,54 @@ export default function Sponsors() {
 						alt="Trivago"
 						width="102"
 						height="32"
+						style="height: 3rem; width: auto;"
+					/>
+				</a>
+			</li>
+			<li class={styles.sponsorItem}>
+				<a
+					href="https://scams.info"
+					title="scams.info"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={ScamsInfo}
+						alt="Scams.info"
+						width="240"
+						height="240"
+						style="height: 3rem; width: auto;"
+					/>
+				</a>
+			</li>
+			<li class={styles.sponsorItem}>
+				<a
+					href="https://deno.land"
+					title="Deno"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={DenoSvg}
+						alt="Deno"
+						width="813"
+						height="813"
+						style="height: 3rem; width: auto;"
+					/>
+				</a>
+			</li>
+			<li class={styles.sponsorItem}>
+				<a
+					href="https://songsterr.com"
+					title="Songsterr"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={SongsterrSvg}
+						alt="Songsterr"
+						width="26"
+						height="30"
 						style="height: 3rem; width: auto;"
 					/>
 				</a>
