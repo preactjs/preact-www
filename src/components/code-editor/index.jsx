f32f1dfa0714e22f48aabe37b26522233d622ea7
Ryan Christian <rchristian@ryanchristian.dev>
2024-01-17 22:26:02 -0600

    refactor: Re-enabled REPL & Tutorial, seemingly all "working"

--- src/components/code-editor/index.jsx
+++ src/components/code-editor/index.jsx
@@ -5,6 +5,8 @@ import 'codemirror/mode/jsx/jsx';
 import 'codemirror/addon/comment/comment';
 import 'codemirror/lib/codemirror.css';
 import style from './style.module.css';
+// TODO: Fix me, Vite doesn't seem to like :global in CSS Modules
+import './temp.css';
 
 export default class CodeEditor extends Component {
 	scratch = document.createElement('div');
