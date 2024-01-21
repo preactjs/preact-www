a48b5405749f079b0320ce3f55dae90475692a23
Ryan Christian <rchristian@ryanchristian.dev>
2024-01-21 02:52:24 -0600

    refactor: Skip stripping header from markdown content

--- preact.config.js
+++ preact.config.js
@@ -98,7 +98,6 @@ export default function (config, env, helpers) {
 					if (!meta.title) {
 						let [, title] = content.match(TITLE_REG) || [];
 						if (title) {
-							content = content.replace(TITLE_REG, '');
 							meta.title = title;
 						}
 					}
