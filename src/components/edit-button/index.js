09593e77e583f40746dc277654f3d03501f8ecaa
Ryan Christian <rchristian@ryanchristian.dev>
2024-02-12 00:24:42 -0600

    refactor: Touch up lang fallback message & style

--- src/components/edit-button/index.js
+++ src/components/edit-button/index.js
@@ -11,20 +11,23 @@ export default function EditThisPage({ isFallback }) {
 	const editUrl = `https://github.com/preactjs/preact-www/tree/master/content/${lang}${path}`;
 	return (
 		<div class={style.wrapper}>
-			<a
-				class={style.edit}
-				href={editUrl}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				{!isFallback ? 'Edit this Page' : 'Add translation'}
-			</a>
-
-			{isFallback && (
+			{!isFallback ? (
+				<a
+					class={style.edit}
+					href={editUrl}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Edit this Page
+				</a>
+			) : (
 				<div class={style.fallback}>
 					<div class={style.fallbackInner}>
 						Could not find a translation for this page. You can help us out by{' '}
-						<a href={editUrl}>adding one here</a>.
+						<a href={editUrl} target="_blank" rel="noopener noreferrer">
+							adding one here
+						</a>
+						.
 					</div>
 				</div>
 			)}
