b66acb354a3f1bc4b4ae5f74a2f3cf2350f00fcb
Ryan Christian <rchristian@ryanchristian.dev>
2024-02-16 02:56:05 -0600

    chore: Copy over RSS plugin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import preact from '@preact/preset-vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 import yaml from 'yaml';
+import { Feed } from 'feed';
+import config from './src/config.json';
 
 // Find YAML FrontMatter preceeding a markdown document
 const FRONT_MATTER_REG = /^\s*---\n\s*([\s\S]*?)\s*\n---\n/i;
@@ -22,7 +24,11 @@ export default defineConfig({
 				enabled: true,
 				renderTarget: '#app',
 				// The routes that will not be discovered automatically
-				additionalPrerenderRoutes: ['/404', '/guide/v8/getting-started', '/branding']
+				additionalPrerenderRoutes: [
+					'/404',
+					'/guide/v8/getting-started',
+					'/branding'
+				]
 			},
 			devToolsEnabled: false
 		}),
@@ -39,10 +45,11 @@ export default defineConfig({
 						const matches = content.match(FRONT_MATTER_REG);
 						if (!matches) return content;
 
-
 						let meta;
 						try {
-							meta = yaml.parse('---\n' + matches[1].replace(/^/gm, '  ') + '\n') || {};
+							meta =
+								yaml.parse('---\n' + matches[1].replace(/^/gm, '  ') + '\n') ||
+								{};
 						} catch (e) {
 							throw new Error(`Error parsing YAML FrontMatter in ${path}`);
 						}
@@ -69,6 +76,66 @@ export default defineConfig({
 				{ src: 'src/robots.txt', dest: './' },
 				{ src: 'src/_headers', dest: './' }
 			]
-		})
+		}),
+		rssFeedPlugin()
 	]
 });
+
+/**
+ * @returns {import('vite').Plugin}
+ */
+function rssFeedPlugin() {
+	return {
+		name: 'rss-feed',
+		apply: 'build',
+		generateBundle() {
+			const feed = new Feed({
+				title: 'Preact Blog',
+				description: 'Preact news and articles',
+				id: 'https://preactjs.com',
+				link: 'https://preactjs.com',
+				language: 'en',
+				image: 'https://preactjs.com/assets/branding/symbol.png',
+				favicon: 'https://preactjs.com/favicon.ico',
+				copyright: 'All rights reserved 2022, the Preact team',
+				feedLinks: {
+					json: 'https://preactjs.com/json',
+					atom: 'https://preactjs.com/atom'
+				}
+			});
+
+			config.blog.forEach(post => {
+				feed.addItem({
+					title: post.name.en,
+					id: `https://preactjs.com${post.path}`,
+					link: `https://preactjs.com${post.path}`,
+					description: post.excerpt.en,
+					date: new Date(post.date)
+				});
+			});
+
+			function removeDefaultGenerator(str) {
+				return str
+					.split('\n')
+					.filter(
+						line =>
+							line !==
+							'<generator>https://github.com/jpmonette/feed</generator>'
+					)
+					.join('\n');
+			}
+
+			this.emitFile({
+				type: 'asset',
+				fileName: 'feed.xml',
+				source: removeDefaultGenerator(feed.rss2())
+			});
+
+			this.emitFile({
+				type: 'asset',
+				fileName: 'feed.atom',
+				source: removeDefaultGenerator(feed.atom1())
+			});
+		}
+	};
+}
