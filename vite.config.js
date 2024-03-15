407ddf7813e4e49c6f13673149441449994a2c8e
Ryan Christian <rchristian@ryanchristian.dev>
2024-03-15 03:04:30 -0500

    refactor: Markdown plugin config, ensure _redirects is copied

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,45 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import replace from '@rollup/plugin-replace';
-
 import yaml from 'yaml';
 import { Feed } from 'feed';
 import config from './src/config.json';
 
-// Find YAML FrontMatter preceeding a markdown document
-const FRONT_MATTER_REG = /^\s*---\n\s*([\s\S]*?)\s*\n---\n/i;
+/**
+ * @param {string} content
+ * @param {string} path
+ * @returns {string}
+ */
+function compileMarkdown(content, path) {
+	// Find YAML FrontMatter preceeding a markdown document
+	const FRONT_MATTER_REG = /^\s*---\n\s*([\s\S]*?)\s*\n---\n/i;
 
-// Find a leading title in a markdown document
-const TITLE_REG = /^\s*#\s+(.+)\n+/;
+	// Find a leading title in a markdown document
+	const TITLE_REG = /^\s*#\s+(.+)\n+/;
+
+	const matches = content.match(FRONT_MATTER_REG);
+	if (matches) {
+		let meta;
+		try {
+			meta =
+				yaml.parse('---\n' + matches[1].replace(/^/gm, '  ') + '\n') ||
+				{};
+		} catch (e) {
+			throw new Error(`Error parsing YAML FrontMatter in ${path}`);
+		}
+		content = content.replace(FRONT_MATTER_REG, '');
+		if (!meta.title) {
+			let [, title] = content.match(TITLE_REG) || [];
+			if (title) {
+				meta.title = title;
+			}
+		}
+
+		content = '---\n' + JSON.stringify(meta) + '\n---\n' + content;
+	}
+
+	return content;
+}
 
 export default defineConfig({
 	publicDir: 'src/assets',
@@ -42,33 +71,7 @@ export default defineConfig({
 				{
 					src: './content/**/*.md',
 					dest: './',
-					transform: (content, path) => {
-						if (typeof content !== 'string') {
-							content = content.toString('utf8');
-						}
-
-						const matches = content.match(FRONT_MATTER_REG);
-						if (!matches) return content;
-
-						let meta;
-						try {
-							meta =
-								yaml.parse('---\n' + matches[1].replace(/^/gm, '  ') + '\n') ||
-								{};
-						} catch (e) {
-							throw new Error(`Error parsing YAML FrontMatter in ${path}`);
-						}
-						content = content.replace(FRONT_MATTER_REG, '');
-						if (!meta.title) {
-							let [, title] = content.match(TITLE_REG) || [];
-							if (title) {
-								meta.title = title;
-							}
-						}
-
-						content = '---\n' + JSON.stringify(meta) + '\n---\n' + content;
-						return content;
-					}
+					transform: compileMarkdown
 				}
 			],
 			structured: true,
@@ -79,7 +82,8 @@ export default defineConfig({
 		viteStaticCopy({
 			targets: [
 				{ src: 'src/robots.txt', dest: './' },
-				{ src: 'src/_headers', dest: './' }
+				{ src: 'src/_headers', dest: './' },
+				{ src: 'src/_redirects', dest: './' }
 			]
 		}),
 		rssFeedPlugin()
