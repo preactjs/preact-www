131436d567d36d48a87c0472d0ed9f3c951eeb74
Ryan Christian <rchristian@ryanchristian.dev>
2024-04-01 22:34:25 -0500

    refactor: Make docsearch links relative for dev & previews

--- src/components/header/search.jsx
+++ src/components/header/search.jsx
@@ -5,7 +5,10 @@ import { DocSearch } from '@docsearch/react';
 import './docsearch.css';
 
 // Might be a problem with the Algolia data, but it appends `#app` to all URLs without a hash fragment
-const transformItems = (items) => items.map(i => Object.assign(i, { url: i.url.replace(/#app$/, '') }));
+const transformItems = (items) =>
+	items.map(i =>
+		Object.assign(i, { url: new URL(i.url).pathname.replace(/#app$/, '') })
+	);
 
 export default function Search() {
 	return (
