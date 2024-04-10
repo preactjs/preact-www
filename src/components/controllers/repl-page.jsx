ff0fb66168b7138499965a267bb6bc1a7d14b817
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2024-04-10 13:24:55 -0500

    fix: REPL shell & code query param

--- src/components/controllers/repl-page.jsx
+++ src/components/controllers/repl-page.jsx
@@ -4,11 +4,12 @@ import { useTitle, useDescription } from './utils';
 import { useLanguage } from '../../lib/i18n';
 
 export default function ReplPage() {
+	const { query } = useRoute();
 	const [lang] = useLanguage();
 
 	const { meta } = useContent([lang, 'repl']);
 	useTitle(meta.title);
 	useDescription(meta.description);
 
-	return <Repl />;
+	return <Repl query={query} />;
 }
