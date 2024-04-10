5876c769292eb9a12d49473175ac03afb71f65ea
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2024-04-10 13:49:49 -0500

    fix: REPL shell & code query param (#1119)
    * fix: REPL shell & code query param
    
    * fix: Missing import
    
    * ci: fix

--- src/components/controllers/repl-page.jsx
+++ src/components/controllers/repl-page.jsx
@@ -1,14 +1,16 @@
+import { useRoute } from 'preact-iso';
 import { Repl } from './repl';
 import { useContent } from '../../lib/use-resource';
 import { useTitle, useDescription } from './utils';
 import { useLanguage } from '../../lib/i18n';
 
 export default function ReplPage() {
+	const { query } = useRoute();
 	const [lang] = useLanguage();
 
 	const { meta } = useContent([lang, 'repl']);
 	useTitle(meta.title);
 	useDescription(meta.description);
 
-	return <Repl />;
+	return <Repl query={query} />;
 }
