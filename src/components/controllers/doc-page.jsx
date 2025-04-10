5c2ca5f069c333b49df87239a6298673ec004ec4
Ryan Christian <rchristian@ryanchristian.dev>
2025-04-09 23:41:43 -0500

    feat: Warn users for unmaintained translations

--- src/components/controllers/doc-page.jsx
+++ src/components/controllers/doc-page.jsx
@@ -1,5 +1,6 @@
 import { useRoute } from 'preact-iso';
 import { useContent } from '../../lib/use-content';
+import { useLanguage } from '../../lib/i18n.jsx';
 import config from '../../config.json';
 import { NotFound } from './not-found';
 import cx from '../../lib/cx';
@@ -35,6 +36,7 @@ export function DocLayout({ isGuide = false }) {
 				)}
 				<div class={style.inner}>
 					{isGuide && <OldDocsWarning />}
+					{isGuide && <UnmaintainedTranslationWarning meta={meta} />}
 					<MarkdownRegion html={html} meta={meta} />
 					<Footer />
 				</div>
@@ -55,7 +57,7 @@ function OldDocsWarning() {
 	);
 
 	return (
-		<div class={style.oldDocsWarning}>
+		<div class={style.stickyWarning}>
 			You are viewing the documentation for an older version of Preact.
 			{latestExists ? (
 				<>
@@ -71,6 +73,46 @@ function OldDocsWarning() {
 					.
 				</>
 			)}
+		</div>
+	);
+}
+
+// Maybe include zh? It's received some contributions recently
+const MAINTAINED_LANGUAGES = ['en', 'ru'];
+function UnmaintainedTranslationWarning({ meta }) {
+	const { path, params } = useRoute();
+	const { name, version } =params;
+	const [lang, setLang] = useLanguage();
+
+	if (version !== LATEST_MAJOR || MAINTAINED_LANGUAGES.includes(lang) || meta.isFallback) {
+		return null;
+	}
+
+	const editUrl = `https://github.com/preactjs/preact-www/tree/master/content/${lang}${path}.md`;
+
+	return (
+		<div class={style.stickyWarning}>
+			You are viewing the documentation for an unmaintained translation of Preact.
+			Whilst we try to offer these docs in as many languages as possible, we rely upon
+			our community members to help us maintain them. This translation has seen little
+			maintenance in recent months and may have fallen out of sync with the English version.
+
+			<br />
+			<div class={style.unmaintaindTranslationLinks}>
+				<a
+					href={`/guide/${LATEST_MAJOR}/${name}`}
+					onClick={() => setLang('en')}
+				>
+					Switch to the English version
+				</a>
+				<a
+					href={editUrl}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Help us maintain this translation
+				</a>
+			</div>
 		</div>
 	);
 }
