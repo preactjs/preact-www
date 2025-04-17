4ef71fbf7d25132376118fd901a42f2819884792
Ryan Christian <rchristian@ryanchristian.dev>
2025-04-17 01:46:07 -0500

    chore: Remove ref forwarding now that iso supports it

--- src/lib/repl.js
+++ src/lib/repl.js
@@ -1,20 +1,7 @@
-import { options } from 'preact';
 import { lazy } from 'preact-iso';
 
 const CodeEditor = lazy(() => import('../components/code-editor'));
 const Runner = lazy(() => import('../components/controllers/repl/runner'));
-
-// `preact-iso` doesn't forward refs to lazy-loaded components (should we?)
-// so we need to manually do it here to support the tutorial which reads the runner ref
-const oldDiff = options.__b;
-options.__b = (vnode) => {
-	if (vnode.type === Runner && vnode.ref) {
-		vnode.props.ref = vnode.ref;
-		vnode.ref = null;
-	}
-
-	if (oldDiff) oldDiff(vnode);
-};
 
 /**
  * @returns {void}
