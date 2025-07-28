2e999913c8c84deb5a4515f0dbb304fda113cbee
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2025-07-28 02:43:06 -0500

    chore: Sort translations, fix HTML error in branding (#1314)

--- src/components/branding/index.jsx
+++ src/components/branding/index.jsx
@@ -3,36 +3,23 @@ import config from '../../config.json';
 
 export default function Branding() {
 	return (
-		<section class={style.logos}>
+		<div class={style.logos}>
 			{config.branding.map(asset => (
 				<LogoVariation name={asset.name} alt={asset.alt} />
 			))}
-		</section>
+		</div>
 	);
 }
 
 function LogoVariation({ name, alt }) {
 	return (
 		<div class={style.variation}>
-			<img
-				src={`/branding/${name}.svg`}
-				alt={alt}
-				loading="lazy"
-				height="64"
-			/>
+			<img src={`/branding/${name}.svg`} alt={alt} loading="lazy" height="64" />
 			<div class={style.links}>
-				<a
-					href={`/branding/${name}.svg`}
-					target="_blank"
-					rel="noreferrer"
-				>
+				<a href={`/branding/${name}.svg`} target="_blank" rel="noreferrer">
 					SVG
 				</a>
-				<a
-					href={`/branding/${name}.png`}
-					target="_blank"
-					rel="noreferrer"
-				>
+				<a href={`/branding/${name}.png`} target="_blank" rel="noreferrer">
 					PNG
 				</a>
 			</div>
