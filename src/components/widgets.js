5c1567aac5d23f64b2d3f9855f78429b7aa0513b
Ryan Christian <rchristian@ryanchristian.dev>
2025-10-10 00:12:05 -0500

    junk: Check build sizes for lazy loading widgets

--- src/components/widgets.js
+++ src/components/widgets.js
@@ -1,13 +1,21 @@
+import { lazy } from 'preact-iso';
+
 import Jumbotron from './jumbotron';
 import GithubRepos from './github-repos';
 import TodoList from './todo-list';
 import Logo from './logo';
 import Toc from './table-of-contents';
-import BlogOverview from './blog-overview';
 import Sponsors from './sponsors';
-import WeAreUsing from './we-are-using';
-import Branding from './branding';
 import TabGroup from './tab-group';
+
+// Hoist the CSS to avoid FOUC
+import './branding/style.module.css';
+import './blog-overview/style.module.css';
+import './we-are-using/style.module.css';
+
+const Branding = lazy(() => import('./branding'));
+const BlogOverview = lazy(() => import('./blog-overview'));
+const WeAreUsing = lazy(() => import('./we-are-using'));
 
 export default {
 	Toc,
